5ee09f06ddff428bfb329ff6e0db3947cb6095eb
florrup <florencia441@hotmail.com>
2018-01-20 08:56:45 -0300

    Implementing login and register listeners
    Validating credentials during the login, displaying progress dialog while
    wainting for server response, starting to work on the register activity

--- app/src/main/java/ar/uba/fi/carpooling/MainActivity.java
+++ app/src/main/java/ar/uba/fi/carpooling/MainActivity.java
@@ -1,26 +1,108 @@
 package ar.uba.fi.carpooling;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginBtn;
+    private Button registerBtn;
+    private ProgressDialog progressDialog;
+    private EditText emailLoginText;
+    private EditText passwordLoginText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button loginBtn = (Button) findViewById(R.id.loginBtn);
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+        registerBtn = (Button) findViewById(R.id.registerBtn);
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText usernameLoginText = (EditText) findViewById(R.id.emailLoginText);
-                EditText passwordLoginText = (EditText) findViewById(R.id.passwordLoginText);
+                login();
+            }
+        });
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(registerIntent);
             }
         });
     }
+
+    private boolean validateCredentials() {
+        String email = emailLoginText.getText().toString();
+        String password = passwordLoginText.getText().toString();
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailLoginText.setError("Email inválido");
+            return false;
+        }
+
+        if (password.length() < 8) {
+            passwordLoginText.setError("Contraseña inválida");
+            return false;
+        }
+        return true;
+    }
+
+    private void login() {
+        emailLoginText = (EditText) findViewById(R.id.emailLoginText);
+        passwordLoginText = (EditText) findViewById(R.id.passwordLoginText);
+        loginBtn.setEnabled(false); // so that the user won't click the button again
+
+        if (!validateCredentials()) {
+            Toast.makeText(MainActivity.this, "Credenciales inválidas", Toast.LENGTH_SHORT).show();
+            loginBtn.setEnabled(true);
+            return;
+        }
+
+        // Show a ProgressDialog in this Thread
+        progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setTitle("Autenticando...");
+        progressDialog.setMessage("Por favor, espere.");
+        progressDialog.show();
+
+        String email = emailLoginText.getText().toString();
+        String password = passwordLoginText.getText().toString();
+
+        // New Thread that will be in charge of the login
+        new LoginTaskThread().execute("Parámetros que necesite el LoginTaskThread");
+    }
+
+    public void displayResult(String textoAMostrar){
+        emailLoginText.setText(textoAMostrar);
+    }
+
+    private class LoginTaskThread extends AsyncTask {
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            // Pretend to fetch credentials
+            for (int i=1;i<40000; i++) {
+                Log.i("Login", "Probando");
+            }
+            return "Resultado";
+        }
+        @Override
+        protected void onPostExecute(Object result) {
+            displayResult((String)result);
+            if (progressDialog != null) {
+                progressDialog.dismiss();
+            }
+        }
+    }
 }
+
--- app/src/main/java/ar/uba/fi/carpooling/SignUpActivity.java
+++ app/src/main/java/ar/uba/fi/carpooling/SignUpActivity.java
@@ -1,7 +1,10 @@
 package ar.uba.fi.carpooling;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        final Button finishRegisterBtn = (Button) findViewById(R.id.finishRegisterBtn);
+        finishRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // TODO check registration information
+
+                Intent loginIntent = new Intent(SignUpActivity.this, MainActivity.class);
+                startActivity(loginIntent);
+            }
+        });
     }
 }
